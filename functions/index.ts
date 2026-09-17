26d6d4c058a3cf83e6f0cf09c9a25aead174c8e1
wwz554 <83067439+wwz554@users.noreply.github.com>
2026-09-17 15:45:26 +0800

    point Pages root function at production types

--- functions/index.ts
+++ functions/index.ts
@@ -1,5 +1,5 @@
 import { handlePagesRequest } from '../src/pages';
-import type { Env } from '../src/worker';
+import type { Env } from '../src/types';
 
 export async function onRequest(context: { request: Request; env: Env }): Promise<Response> {
   return handlePagesRequest(context.request, context.env);
