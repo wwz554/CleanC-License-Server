e41fbde4b64b0f7281b438b98e8a68309383cf82
wwz554 <83067439+wwz554@users.noreply.github.com>
2026-09-17 17:11:56 +0800

    route all pages requests through production gateway

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,6 @@
-import { handlePagesRequest } from '../src/pages';
+import { handleProductionRequest } from '../src/production';
 import type { Env } from '../src/worker';
 
 export async function onRequest(context: { request: Request; env: Env }): Promise<Response> {
-  return handlePagesRequest(context.request, context.env);
+  return handleProductionRequest(context.request, context.env);
 }
